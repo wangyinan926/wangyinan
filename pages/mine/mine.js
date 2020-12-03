245a2f1d82562abf38fc0ee16b5c2b1d1d23e9f3
王银安 <wangyinan@youngzone.bet>
2020-12-03 17:22:51 +0800

    个人资料

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,14 +5,52 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    age:0,
+    phone: '13243341305',
+    name: '王银安'
   },
-
+  call: function () {
+    console.log(this.data.phone)
+    var that = this
+    wx.makePhoneCall({
+      phoneNumber: that.data.phone,
+      success: function () {
+        console.log('成功')
+      }
+    })
+  },
+  add: function () {
+    var that = this
+    wx.addPhoneContact({
+      firstName: that.data.name,
+      mobilePhoneNumber: that.data.phone,
+      success: function () {
+        console.log('成功')
+      }
+    })
+  },
+  copyText: function () {
+    wx.setClipboardData({
+      data:this.data.phone ,
+      success: function (res) {
+        wx.hideToast()
+        wx.showModal({
+          title: '提示',
+          content:'微信号已经复制,请到微信页面添加好友',
+          showCancel:false
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      var year = new Date().getFullYear()
+      console.log(year)
+      this.setData({
+        age: year - 1996
+      })
   },
 
   /**
